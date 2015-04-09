328c03899e12e5396688f26fff87f090841ad098
Felix Guerrero <felixcriv@gmail.com>
2015-04-08 21:02:33 -0400

    app.js created

---
+++ public/app.js
@@ -0,0 +1,51 @@
+ (function() {
+
+     var spinner = document.querySelector('paper-spinner');
+     var template = document.getElementById('events');
+     template.bindingDelegate = new PolymerExpressions;
+
+     function fulfill(response) {
+         return response.response.events
+     }
+
+     function processDates(data) {
+         var newData = [];
+         data.forEach(function(value) {
+             value.ocurrido = formatDate(value.fecha + ' ' + value.date);
+             newData.push(value);
+         });
+
+         return newData;
+     }
+
+     function bindTemplate(data) {
+         template.model = {
+             response: data
+         };
+     }
+
+     function rejected(error) {
+         console.error(error);
+     }
+
+     function completed(data) {
+         spinner.active = false;
+     }
+
+     function formatDate(date) {
+         var eventTime = moment(date, 'DD-MM-YYYY HH:mm').utcOffset(-30);
+         return eventTime.startOf('hour').fromNow();
+     }
+
+     function polymerReady() {
+         var ajax = document.querySelector('ajax-promise');
+         ajax.request()
+             .then(fulfill, rejected)
+             .then(processDates)
+             .then(bindTemplate)
+             .then(completed);
+     }
+
+     document.addEventListener('polymer-ready', polymerReady);
+
+ })();
